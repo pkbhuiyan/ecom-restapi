de0f3a5d3b6133c31da91bd258221a3fb251ea11
Pranta <pkbhuiyan@gmail.com>
2019-05-29 00:54:15 +0600

    update

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'detail' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+        'price' => $faker->numberBetween($min = 10, $max = 999),
+        'stock' => $faker->randomDigit,
+        'discount' => $faker->numberBetween($min = 4, $max = 10),
+        
     ];
 });
